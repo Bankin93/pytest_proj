150f1c9a614b6b7e1f888e91155359851a23e01b
Bankin93 <igorkonov1993@gmail.com>
2023-01-30 21:13:57 +0300

    улучшил тесты для test_arrs.py через pytest.mark.parametrize

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,14 +1,20 @@
+import pytest
 from utils import arrs
 
 
-def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 2
-    assert arrs.get([], -1, "test") == "test"
+@pytest.mark.parametrize(
+    "array, index, default, result",
+    [([1, 2, 3], 1, "test", 2), ([], -1, "test", "test")]
+)
+def test_get(array, index, default, result):
+    assert arrs.get(array, index, default) == result
 
 
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
-    assert arrs.my_slice([], 1) == []
-    assert arrs.my_slice([1, 2, 3], -2) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], -5) == [1, 2, 3]
+@pytest.mark.parametrize(
+    "coll, start, end, result",
+    [([1, 2, 3, 4], 1, 3, [2, 3]), ([1, 2, 3], 1, None, [2, 3]),
+     ([], 1, None, []), ([1, 2, 3], -2, None, [2, 3]),
+     ([1, 2, 3], -5, None, [1, 2, 3])]
+)
+def test_slice(coll, start, end, result):
+    assert arrs.my_slice(coll, start, end) == result
